557a0d79b5e487deac8040f1c40bf02f48dfaa60
XiyuChen <xyuChen@thoughtworks.com>
2017-11-28 13:10:09 +0800

    [xy] add tmux conf

--- install.sh
+++ install.sh
@@ -6,3 +6,7 @@ DOTFILE_HOME="$(cd $(dirname $0))$(pwd)"
 # spacemacs dotfile
 echo "--------------installing spacemacs config------------------"
 ln -sf "$DOTFILE_HOME/emacs/spacemacs.symlink" "$HOME_DIR/.spacemacs" && echo "---done---"
+
+# tmux dotfile
+echo "--------------installing tmux config ----------------------"
+ln -sf "$DOTFILE_HOME/tmux.conf" "$HOME_DIR/.tmux.conf" && echo "---done---"
