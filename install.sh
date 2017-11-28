cdd589ccba0260fdff67416e3c8f616c940f693f
XiyuChen <xyuChen@thoughtworks.com>
2017-11-28 13:06:21 +0800

    [xy] update spacemacs

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+HOME_DIR=$1
+[ -z $HOME_DIR ] && echo 'please pass HOME directory through argument: install ~ ' && exit
+DOTFILE_HOME="$(cd $(dirname $0))$(pwd)"
 
 # spacemacs dotfile
 echo "--------------installing spacemacs config------------------"
-[ -e ~/.spacemacs ] && rm ~/.spacemacs && echo "---remove original one"
-ln $(pwd)/spacemacs ~/.spacemacs && echo "---done"
+ln -sf "$DOTFILE_HOME/emacs/spacemacs.symlink" "$HOME_DIR/.spacemacs" && echo "---done---"
